c21a3d37cd8d3bbaa6c188d08956b32a6b2e08e0
Barbora Kubalcova <barbora.kubalcova@student.tuke.sk>
2022-11-03 09:51:10 +0100

    try

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<style>
- 
-h2{
-         
-color:blue;
-}
- 
- 
-</style>
-
-
-<h2>Hello</h2>
+<?php
+        echo '<h1>getParameters page</h1>';
+        
+        $sn1 = $_GET["a"];
+        $sn2 = $_GET["b"];
+        
+        $text = "a=" . $sn1 . " b=" . $sn2;
+        $sum = $sn1 + $sn2;
+        
+        echo $text;
+        echo "<br>";
+        echo "Sum:" . $sum;
+    ?>
