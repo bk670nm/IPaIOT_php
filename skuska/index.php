dd10bf6935536a087704b3bee3a2bb106b532884
Barbora Kubalcova <barbora.kubalcova@student.tuke.sk>
2022-11-17 18:05:00 +0100

    skuska

---
+++ skuska/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>LOGIN</h1>
+    <form name="Formular" method="POST" action="login.php">			
+	
+	<label for="meno">Username:</label>
+	<input type="text" name="username">
+	
+	<br><br>
+	<label for="meno">Password:</label>
+	<input type="password" name="password">
+	
+	<br><br>
+	<input type="submit" value="LOGIN">
+</form>
+</body>
+</html>
