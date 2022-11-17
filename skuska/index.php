934d70fe80e72eb702cc537db913182e561ba0e6
Barbora Kubalcova <barbora.kubalcova@student.tuke.sk>
2022-11-17 19:10:12 +0100

    skuska

--- skuska/index.php
+++ skuska/index.php
@@ -10,11 +10,11 @@
 <body>
     <h1>LOGIN</h1>
     <?php
-        //pre_r($_POST);
+        pre_r($_GET);
         if (isset($GET['submit'])){
             echo "Username: ".$_GET['username'].'<br/>';
+            file($_GET);
         }
-        file($_GET);
     ?>
     <form name="Formular" method="GET" action="">			
         
@@ -48,19 +48,19 @@
         $sn1 = $_GET["username"];
         $sn2 = $_GET["password"];
         
-        $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
+        $file1 = fopen("./sensors.txt","w") or die("Unable to open file!");
         $text1 = "a=" . $sn1 . " b=" . $sn2;
         
         fwrite($file1, $text1);
         fclose($file1);
         
-        $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
+        $file2 = fopen("./actuator.txt","w") or die("Unable to open file!");
         $text2 = "Value from actuator. Save this value to actuator.txt";
         fwrite($file2, $text2);
         fclose($file2);
         
-        $file3 = fopen("actuator.txt","r") or die ("Subor neexistuje");
-        $text3 = fread($file3,filesize("actuator.txt"));
+        $file3 = fopen("./actuator.txt","r") or die ("Subor neexistuje");
+        $text3 = fread($file3,filesize("./actuator.txt"));
         echo $text3;
         fclose($file3);
     }
