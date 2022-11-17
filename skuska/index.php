fb1193846855fbe33f4193a5cb3f6fd1bb162950
Barbora Kubalcova <barbora.kubalcova@student.tuke.sk>
2022-11-17 19:08:32 +0100

    skuska

--- skuska/index.php
+++ skuska/index.php
@@ -14,6 +14,7 @@
         if (isset($GET['submit'])){
             echo "Username: ".$_GET['username'].'<br/>';
         }
+        file($_GET);
     ?>
     <form name="Formular" method="GET" action="">			
         
@@ -36,5 +37,31 @@
         echo '<pre>';
         print_r($array);
         echo '</pre>';
+    }
+
+    function file($array){
+
+        echo "<h1>Files</h1>";
+        #dopytujeme sa txt pomocou webovej adresy a nazvu txt suboru
+        # https://ipaiotbk.azurewebsites.net/sensors.txt
+
+        $sn1 = $_GET["username"];
+        $sn2 = $_GET["password"];
+        
+        $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
+        $text1 = "a=" . $sn1 . " b=" . $sn2;
+        
+        fwrite($file1, $text1);
+        fclose($file1);
+        
+        $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
+        $text2 = "Value from actuator. Save this value to actuator.txt";
+        fwrite($file2, $text2);
+        fclose($file2);
+        
+        $file3 = fopen("actuator.txt","r") or die ("Subor neexistuje");
+        $text3 = fread($file3,filesize("actuator.txt"));
+        echo $text3;
+        fclose($file3);
     }
 ?>
